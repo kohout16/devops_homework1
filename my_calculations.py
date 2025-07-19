bd2e28f74f323d84c3e23c6fff7602f1056995bc
kohout <laz.jiri@gmail.com>
2025-07-19 08:22:28 +0200

    python test

---
+++ my_calculations.py
@@ -0,0 +1,9 @@
+# project/code/my_calculations.py
+
+class Calculations:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def get_sum(self):
+        return self.a + self.b
