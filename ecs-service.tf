428d17ba96875dc7710b0aa18317db70f68ed698
kohout <laz.jiri@gmail.com>
2025-08-10 13:37:48 +0200

    execution role added

--- ecs-service.tf
+++ ecs-service.tf
@@ -9,6 +9,8 @@ resource "aws_ecs_task_definition" "lesson7" {
   cpu                      = "512"
   memory                   = "1024"
 
+  execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
+
   container_definitions = jsonencode([
     {
       name      = "web"
