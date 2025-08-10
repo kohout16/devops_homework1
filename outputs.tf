58c8ca44f2604801b5a65765aadecfbf862d7261
kohout <laz.jiri@gmail.com>
2025-08-10 15:10:59 +0200

    outputs remove ec2 instance

--- outputs.tf
+++ outputs.tf
@@ -1,22 +1,22 @@
-output "instance_id" {
-  description = "ID vytvořené EC2 instance"
-  value       = aws_instance.ec2_instance.id
-}
+# output "instance_id" {
+#   description = "ID vytvořené EC2 instance"
+#   value       = aws_instance.ec2_instance.id
+# }
 
-output "public_ip" {
-  description = "Veřejná IP adresa EC2 instance"
-  value       = aws_instance.ec2_instance.public_ip
-}
+# output "public_ip" {
+#   description = "Veřejná IP adresa EC2 instance"
+#   value       = aws_instance.ec2_instance.public_ip
+# }
 
-output "public_dns" {
-  description = "Veřejný DNS název EC2 instance"
-  value       = aws_instance.ec2_instance.public_dns
-}
+# output "public_dns" {
+#   description = "Veřejný DNS název EC2 instance"
+#   value       = aws_instance.ec2_instance.public_dns
+# }
 
-output "ssh_command" {
-  description = "Příkaz pro SSH připojení k instanci"
-  value       = "ssh -i ${var.public_key_path} ec2-user@${aws_instance.ec2_instance.public_ip}"
-}
+# output "ssh_command" {
+#   description = "Příkaz pro SSH připojení k instanci"
+#   value       = "ssh -i ${var.public_key_path} ec2-user@${aws_instance.ec2_instance.public_ip}"
+# }
 
 output "ecr_repository_url" {
 description = "URL of the ECR repository"
