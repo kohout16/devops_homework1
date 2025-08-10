a454c79527be6a2597ca80d492f6f09ee008c309
kohout <laz.jiri@gmail.com>
2025-08-10 15:41:35 +0200

    security groups already defined

--- securitygroups.tf
+++ securitygroups.tf
@@ -1,48 +1,48 @@
-# Security Groups
-resource "aws_security_group" "alb" {
-  name        = "${var.project_name}-alb-sg2"
-  description = "Security group for ALB"
-  vpc_id      = data.aws_vpc.myvpc.id
+# # Security Groups
+# resource "aws_security_group" "alb" {
+#   name        = "${var.project_name}-alb-sg2"
+#   description = "Security group for ALB"
+#   vpc_id      = data.aws_vpc.myvpc.id
   
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   ingress {
+#     from_port   = 80
+#     to_port     = 80
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
   
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
   
-  tags = {
-    Name = "${var.project_name}-alb-sg2"
-  }
-}
+#   tags = {
+#     Name = "${var.project_name}-alb-sg2"
+#   }
+# }
 
-resource "aws_security_group" "ecs_tasks" {
-  name        = "${var.project_name}-ecs-tasks-sg2"
-  description = "Security group for ECS tasks"
-  vpc_id      = data.aws_vpc.myvpc.id
+# resource "aws_security_group" "ecs_tasks" {
+#   name        = "${var.project_name}-ecs-tasks-sg2"
+#   description = "Security group for ECS tasks"
+#   vpc_id      = data.aws_vpc.myvpc.id
   
-  ingress {
-    from_port       = 80
-    to_port         = 80
-    protocol        = "tcp"
-    security_groups = [aws_security_group.alb.id]
-  }
+#   ingress {
+#     from_port       = 80
+#     to_port         = 80
+#     protocol        = "tcp"
+#     security_groups = [aws_security_group.alb.id]
+#   }
   
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
   
-  tags = {
-    Name = "${var.project_name}-ecs-tasks-sg2"
-  }
-}
+#   tags = {
+#     Name = "${var.project_name}-ecs-tasks-sg2"
+#   }
+# }
